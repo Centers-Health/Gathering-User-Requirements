bb560baad79103d9cb1ed48845cb44b6b6d28734
Yossi Sheinfil <28785089+ysheinfil@users.noreply.github.com>
2018-06-08 14:58:59 -0400

    Add model classes

---
+++ UserRequirementsLibrary/UserRequirementsLibrary/Requirement.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserRequirementsLibrary
+{
+    public class Requirement
+    {
+        public int RequirementId {get;set;}
+        public string Details { get; set; }
+        public string Purpose { get; set; }
+        public string SuccessfulWhen { get; set; }
+
+        public int StakeholderId { get; set; }
+        public Stakeholder Stakeholder { get; set; }
+
+        public int ProjectId { get; set; }
+        public Project Project { get; set; }
+    }
+}
